26ddfadfd711d335c4fae8cdb379a97d1a3e0eae
amberwagoner <amberwagoner@outlook.com>
2022-11-01 15:22:44 -0700

    more typos

--- printf_functions.c
+++ printf_functions.c
@@ -32,12 +32,15 @@ int print_c(va_list c)
 int print_s(va_list s)
 {
 	int count;
-	char *str = va_arg(s, char *)
+	char *str = va_arg(s, char *);
 
 	if (str == NULL)
+	{
 		str = "(null)";
+	}
 	for (count = 0; str[count]; count++)
+	{
 		_putchar(str[count]);
-	
+	}
 	return (count);
 }
