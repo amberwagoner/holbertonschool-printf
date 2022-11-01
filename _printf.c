3b3d77fb9304c5747949bd38ae29f24b5d3a4689
amberwagoner <amberwagoner@outlook.com>
2022-10-31 17:57:47 -0700

    first attempt

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+* check - takes format, returns a pointer
+* @format: format specifier
+* Return: Pointer Valid or NULL
+*/
+
+int (*check(const char *format))(va_list)
+{
+	unsigned int i;
+
+	print_format p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{NULL, NULL}
+	}
+
+	for (i = 0; p[i].type != NULL; i++)
+	{
+		if (*p[i].type == *format)
+			break;
+	}
+	return (p[i].f);
+}
+
+/**
+* _printf - prints anything
+* @format: format specifier
+* Return: count of characters
+*/
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list ap;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i];
+			count++;
+		}
+		if (!format[i])
+			return (count);
+
+	f = check(&format[i + 1]);
+	if (f != NULL)
+	{
+		count += f(ap);
+		i += 2;
+		continue;
+	}
+	if (!format[i + 1] == '%')
+		i += 2;
+	else
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
---
+++ printf_functions.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: the character to print
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+* print_c - function
+* @c: char
+* Return: 1
+*/
+int print_c(va_list c)
+{
+	int a = va_arg(c, int);
+
+	_putchar(a);
+	return (1);
+}
+
+/**
+* print_s - function
+* @s: string
+* Return: number of characters
+*/
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *)
+
+	for (count = 0; str[count]; count++)
+		_putchar(str[count]);
+	return (count);
+}
